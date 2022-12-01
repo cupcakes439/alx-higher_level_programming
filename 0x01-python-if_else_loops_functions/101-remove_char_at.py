bafd860092378d8dbd6fd26f8a22965baa6196bd
cupcakes439 <adeolaakande439@gmail.com>
2022-12-01 22:11:53 +0000

    Task 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+uthor - Nwabueze Franklin
+
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])#!/usr/bin/python3
+# Author - Adeola Akande
+
+def remove_char_at(str, n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
