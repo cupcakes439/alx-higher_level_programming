a9aa03aafce85d7b33ef0dad4893f4ca1ee759f7
cupcakes439 <adeolaakande439@gmail.com>
2022-12-01 23:04:31 +0000

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    res = ""
-    argc = len(argv) - 1
-    delim = 's:' if argc > 1 else '.' if argc == 0 else ':'
-    for count, arg in enumerate(argv[1:]):
-        res += '{:d}: {}\n'.format(count+1, arg)
-    print('{:d} argument{}\n{}'.format(argc, delim, res), end="")
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, round(div(a, b))))
