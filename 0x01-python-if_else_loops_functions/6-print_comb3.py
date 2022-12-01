6a0d14b49f64bc4c2f516184021d00f37d1a5680
cupcakes439 <adeolaakande439@gmail.com>
2022-12-01 21:44:03 +0000

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,10 +4,4 @@ for i in range(100):
         print("{}{}".format(int(i / 10), i % 10), end="")
         if (i != 89):
             print(", ", end="")
-print("")#!/usr/bin/python3
-for i in range(100):
-    if int(i / 10) != i % 10 and int(i / 10) < i % 10:
-        print("{}{}".format(int(i / 10), i % 10), end="")
-        if (i != 89):
-            print(", ", end="")
 print("")
